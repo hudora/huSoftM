87fb175cf674770be95949e3c7664333856a338c
m.dornseif@hudora.de <m.dornseif@hudora.de@2a7d6d32-97f8-0310-8fd3-c67d3a265405>
2008-10-20 13:35:40 +0000

    code cleanup doku added sample data added
    git-svn-id: https://cybernetics.hudora.biz/intern/svn/code/projects/huSoftM/trunk@3840 2a7d6d32-97f8-0310-8fd3-c67d3a265405

--- datenexportschnittstelle.py
+++ datenexportschnittstelle.py
@@ -1,4 +1,5 @@
 #!/usr/local/bin/python
+# encoding: utf-8
 
 """ Fix-up of SoftM EDI-Export files for Mosaic.
 
@@ -6,107 +7,89 @@ Created 31.08.2006 by Lars Ronge
 """
 
 import os
-import datetime
 import logging
 import shutil
+import time
 
-sPathHuSoMosa = 'C:/Inhouse/huSoMosa'
-output_dir = 'C:/Inhouse/INVOIC'
-input_dir = 'Q:/OUT'
-
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s %(levelname)s %(message)s',
-                    filename=sPathHuSoMosa + '/logs/INVOIC_Log.txt',
-                    filemode='a+')
-
-logging.debug('Starting Python-Script')
-
-
-if os.path.exists(output_dir + '/lock.lck'):
-    logging.info('Die INVOIC aus SoftM koennen nicht exportiert werden, da im Pfad ' 
-                 + output_dir + ' eine Lock-Datei lock.lck vorhanden ist')
-elif os.path.exists(input_dir + '/lock.lck'):
-    logging.info('Die INVOIC koennen nicht zu Mosaic exportiert werden, da im Pfad '
-                 + input_dir + ' eine Lock-Datei lock.lck vorhanden ist')
-else:
-    # Lock-File in MOSAIC-Pfad schreiben
-    lockfile_mosaic = file(output_dir + '/lock.lck', 'a+')
-    lockfile_mosaic.write(str(datetime.datetime.now()) + ': Auslesen der INVOIC durch ' + __file__)
-    lockfile_mosaic.close()
+def rewrite_records(inputfile, outputfile):
+    """Reads Data confirming to SoftM Datenausgabeschnitstelle from inputfile,
+       transforms it and writes it to outputfile."""
     
-    # Lock-File in SoftM-Pfad schreiben
-    lockfile_softm = file(input_dir + '/lock.lck', 'a+')
-    lockfile_softm.write(str(datetime.datetime.now()) + ': Auslesen der INVOIC durch ' + __file__)
-    lockfile_softm.close()
-    
-    # Dateien im INHOUSE-Verzeichnis auslesen
-    s_aFilesInInhouseDir = os.listdir(input_dir)
-    
-    # Alle Dateien im Verzeichnis durchlaufen
-    for sFileName in s_aFilesInInhouseDir:
-        logging.debug('Ueberpruefung der folgenden Datei: ' + sFileName)
+    outbuf = []
+    # Alle Zeilen parsen 
+    for line in inputfile:
+        # Jeder Zeile ein Leerzeichen voranstellen
+        line = ' ' + line
         
-        # Ist es eine TXT-Datei?
-        if sFileName.lower.endswith('.txt'):
-            sFileNameBase = sFileName[:(len(sFileName) - 4)]
+        if line[20:22] != 'F1':
+            # unbekannte Anpassung - Abkommensnummer?
+            line = line[0:504] + "1" + line[505:]
             
-            # Ist es keine aktualisierte TXT-Datei?
-            if not sFileName.lower.endswith('_updated.txt'):
-                
-                logging.debug("Die Datei " + input_dir + "/" + sFileName + " wird zum Lesen geoeffnet")
-                iInputFile = file(input_dir + '/' + sFileName, 'r')
-                logging.debug("Es wird versucht, die Datei " + sFileNameBase + "_UPDATED.txt anzulegen")
-                iOutputFile = file(sPathHuSoMosa + '/tmp/' + sFileNameBase + '_UPDATED.txt', 'w')
-                
-                logging.debug("Die F1-Zeilen werden angepasst")
-                
-                # Alle Zeilen parsen 
-                for sActualLine in iInputFile:
-                    # Jeder Zeile ein Leerzeichen voranstellen
-                    sActualLine = ' ' + sActualLine
-                    
-                    # Die F1-Zeilen anpassen
-                    #########################
-                    
-                    # Abkommensnummer setzen
-                    if sActualLine[20:22] == 'F1':
-                        sActualLine = sActualLine[0:504] + "1" + sActualLine[505:]
-                    
-                    # Liefertermin setzen (falls nicht gesetzt  
-                    if sActualLine[20:22] == 'F1':
-                        sLiefertermin = sActualLine[70:78]
-                        logging.debug('Liefertermin: ' + sLiefertermin)
-                        if sLiefertermin == '00000000':
-                            sLieferscheindatum = sActualLine[113:121]
-                            logging.debug('Lieferscheindatum: ' + sLieferscheindatum)
-                            sActualLine = sActualLine[0:70] + sLieferscheindatum + sActualLine[78:]
-                        
-                    # Ratio-Nummer aendern
-                    if sActualLine[20:22] == 'F1':
-                        if sActualLine[241:248] == '09504 6' or sActualLine[241:248] == '09504/6':
-                            sActualLine = sActualLine[0:241] + '0950406' + sActualLine[248:]
-                    
-                    # Die Zeile in die UPDATE-Datei schreiben
-                    iOutputFile.write(sActualLine)
-                
-                logging.debug("Schliessen der Dateien")
-                iInputFile.close()
-                iOutputFile.close()
-                
-                shutil.copy(os.path.join(sPathHuSoMosa, 'tmp', sFileNameBase + '_UPDATED.txt'), 
-                            os.path.join(sPathHuSoMosa, 'backup/INVOIC', sFileNameBase + '_UPDATED.txt'))
-                shutil.move(os.path.join(sPathHuSoMosa, 'tmp', sFileNameBase + '_UPDATED.txt'),
-                            os.path.join(output_dir, + sFileNameBase + '_UPDATED.txt'))
-                shutil.move(os.path.join(input_dir, sFileName),
-                            os.path.join(sPathHuSoMosa, 'archive/INVOIC', sFileName))
-                logging.info("Die Datei " + sFileName + " wurde verarbeitet")
+            # Liefertermin auf Lieferscheindatum setzen (falls nicht gesetzt)
+            if line[20:22] == 'F1':
+                liefertermin = line[70:78]
+                if liefertermin == '00000000':
+                    lieferscheindatum = line[113:121]
+                    line = line[0:70] + lieferscheindatum + line[78:]
+                    logging.info('Liefertermin auf Lieferscheindatum %s gesetzt' % lieferscheindatum)
+            
+            # Ratio-Nummer aendern
+            if line[241:248] in ['09504 6', '09504/6']:
+                line = line[0:241] + '0950406' + line[248:]
+                logging.info("Ratio-Nummer geaendert")
+        outbuf.append(line)
     
-    try: 
-        os.remove(output_dir + '/lock.lck')
-    except:
-        logging.error('Lock-Datei konnte nicht entfernt werden: ' + output_dir + '/lock.lck')
-      
-    try:
-        os.remove(input_dir + '/lock.lck')
-    except:
-        logging.error('Lock-Datei konnte nicht entfernt werden: ' + input_dir + '/lock.lck')
+    # Write all outut at once
+    outputfile.write('\n'.join(outbuf))
+    
+
+def handle_file(inpath, outputdir, workdir):
+    """Transform a file by changing some records.
+    
+    This function handles the moving arround of the file."""
+    
+    filename = os.path.split(inpath)
+    guid = "%d-%d" % (os.getpid(), time.time()*1000)
+    updated_filename = '%s_UPDATED.txt' % os.path.splitext(filename)[0]
+    tmp_filename = '%s_UPDATED%s.txt' % (os.path.splitext(filename)[0], guid)
+    tmppath = os.path.join(os.path.join(workdir, 'tmp', tmp_filename))
+    logging.debug('Verarbeitung %r -> %r' % (inpath, tmppath))
+    inputfile = file(inpath, 'r')
+    outputfile = file(tmppath, 'w')
+    
+    logging.debug("Die F1-Zeilen werden angepasst")
+    
+    rewrite_records(inputfile, outputfile)
+    
+    inputfile.close()
+    outputfile.close()
+    
+    # archive original file
+    shutil.move(inpath, os.path.join(workdir, 'archive/INVOIC', filename))
+    # archive updated file
+    shutil.copy(tmppath, os.path.join(workdir, 'backup/INVOIC', updated_filename))
+    # move file to destination
+    shutil.move(tmppath, os.path.join(outputdir, updated_filename))
+    
+    logging.info("Die Datei " + filename + " wurde verarbeitet")
+
+
+def main():
+    """Main function to be called by cron."""
+    workdir = 'C:/Inhouse/huSoMosa'
+    outputdir = 'C:/Inhouse/INVOIC'
+    inputdir = 'Q:/OUT'
+    
+    logging.basicConfig(level=logging.DEBUG,
+                        format='%(asctime)s %(levelname)s %(message)s',
+                        filename=os.path.join(workdir, 'INVOIC_Log.txt'),
+                        filemode='a+')
+    
+    logging.debug("Verarbeite %r nach %r. Logs in %r" % (inputdir, outputdir, workdir))
+    # Alle Dateien im Verzeichnis durchlaufen
+    for filename in os.listdir(inputdir):
+        if filename.lower().endswith('.txt'):
+            handle_file(os.path.join(workdir, filename), outputdir, workdir)
+
+main()
+

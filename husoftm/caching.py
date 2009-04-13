60ea890f91316843f53d02f12dd087ab29f7275e
m.dornseif@hudora.de <m.dornseif@hudora.de@2a7d6d32-97f8-0310-8fd3-c67d3a265405>
2009-04-13 19:02:19 +0000

    don't leave socket sin memcache lying arround
    git-svn-id: https://cybernetics.hudora.biz/intern/svn/code/projects/huSoftM/trunk@5470 2a7d6d32-97f8-0310-8fd3-c67d3a265405

--- husoftm/caching.py
+++ husoftm/caching.py
@@ -11,7 +11,14 @@ Copyright (c) 2009 HUDORA. All rights reserved.
 import memcache
 
 
-# TODO: service basierte benamung memcache.hudora.biz  + monitoring
+
+class Client(memcache.Client):
+    def __del__(self):
+        # see http://code.djangoproject.com/ticket/5133 for the problem we try to solve here
+        self.disconnect_all()
+        return super(Client, self).__del__()
+
+# TODO: service basierte benamung memcache.local.hudora.biz + monitoring
 def get_cache():
-    return memcache.Client(['balancer.local.hudora.biz:11211'])
+    return Client(['balancer.local.hudora.biz:11211'])
 

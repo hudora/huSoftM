1695c97be3aa45260e15cb8ff3562811709e952a
Maximillian Dornseif <md@hudora.de>
2010-11-29 10:05:58 +0100

    info url freigeschaltet

--- softm_express/softmexpress.js
+++ softm_express/softmexpress.js
@@ -46,45 +46,51 @@ var listenport = args[3] || '8082';
 
 httpProxy.createServer(function (req, res) {
   parsedurl = url.parse(req.url);
-  if (parsedurl.pathname != '/sql' || req.method !== 'GET') {
-      res.writeHead(404, {
-                "Content-Type" : 'text/plain',
-                "Server" : "SoftMexpress/Node.js/" + process.version +  " " + process.platform,
-                "Date" : (new Date()).toUTCString()
-            });
-      res.write("Not here!\n");
-      res.end();
+  if (parsedurl.pathname != '/info' || req.method !== 'GET') {
+        req.url = '/info';
+        var proxy = new httpProxy.HttpProxy(req, res);
+        proxy.proxyRequest(destport, desthost);
   } else {
-      hmac = crypto.createHmac('sha1', 'geheim');
-      hmac.update(req.url);
-      digest = hmac.digest(encoding='hex');
-      if (req.headers['x-sig'] != digest) {
-          console.log(req.headers['x-sig']);
-          console.log(digest);
-          res.writeHead(401, {
+      if (parsedurl.pathname != '/sql' || req.method !== 'GET') {
+          res.writeHead(404, {
                     "Content-Type" : 'text/plain',
                     "Server" : "SoftMexpress/Node.js/" + process.version +  " " + process.platform,
                     "Date" : (new Date()).toUTCString()
                 });
-          res.write("Not with me!\n");
+          res.write("Not here!\n");
           res.end();
       } else {
-          query = JSON.parse(querystring.parse(parsedurl.query).q);
-          // build the SQL query
-          querystr = "SELECT " + query.fields.join(',') + " FROM " + query.tablenames.join(',');
-          if (query.condition)
-              querystr = querystr + ' WHERE ' + query.condition.replace(/';/g, "");
-          if (query.grouping)
-              querystr = querystr + ' GROUP BY ' + query.grouping.join(',').replace(/';/g, "");
-          if (query.ordering)
-              querystr = querystr + ' ORDER BY ' + query.ordering.join(',').replace(/';/g, "");
-          if (query.limit)
-              querystr = querystr + 'FETCH FIRST ' + (query.limit + '').replace(/';/g, "") + ' ROWS ONLY';
-          console.log(req.client.remoteAddress + ': ' + querystr);
-          newurl = '/select?' + querystring.stringify({query: querystr, tag: query.tag + '+sEx'})
-          req.url = newurl;
-          var proxy = new httpProxy.HttpProxy(req, res);
-          proxy.proxyRequest(destport, desthost);
+          hmac = crypto.createHmac('sha1', 'geheim');
+          hmac.update(req.url);
+          digest = hmac.digest(encoding='hex');
+          if (req.headers['x-sig'] != digest) {
+              console.log(req.headers['x-sig']);
+              console.log(digest);
+              res.writeHead(401, {
+                        "Content-Type" : 'text/plain',
+                        "Server" : "SoftMexpress/Node.js/" + process.version +  " " + process.platform,
+                        "Date" : (new Date()).toUTCString()
+                    });
+              res.write("Not with me!\n");
+              res.end();
+          } else {
+              query = JSON.parse(querystring.parse(parsedurl.query).q);
+              // build the SQL query
+              querystr = "SELECT " + query.fields.join(',') + " FROM " + query.tablenames.join(',');
+              if (query.condition)
+                  querystr = querystr + ' WHERE ' + query.condition.replace(/';/g, "");
+              if (query.grouping)
+                  querystr = querystr + ' GROUP BY ' + query.grouping.join(',').replace(/';/g, "");
+              if (query.ordering)
+                  querystr = querystr + ' ORDER BY ' + query.ordering.join(',').replace(/';/g, "");
+              if (query.limit)
+                  querystr = querystr + ' FETCH FIRST ' + (query.limit + '').replace(/';/g, "") + ' ROWS ONLY';
+              console.log(req.client.remoteAddress + ': ' + querystr);
+              newurl = '/select?' + querystring.stringify({query: querystr, tag: query.tag + '+sEx'})
+              req.url = newurl;
+              var proxy = new httpProxy.HttpProxy(req, res);
+              proxy.proxyRequest(destport, desthost);
+          }
       }
   }
 }).listen(listenport);

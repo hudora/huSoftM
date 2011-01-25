ac1d11a93944d2cafd3985295af36c4ca0064f59
Christian Klein <c.klein@hudora.de>
2011-01-25 09:29:02 +0100

    Bugfix: Es wird die Zeile mit dem Kommibeleg und nicht die mit dem Lieferschein zurückgegeben

--- husoftm2/kommissionierbelege.py
+++ husoftm2/kommissionierbelege.py
@@ -11,11 +11,6 @@ from husoftm2.lieferscheine import get_ls_kb_data
 from husoftm2.tools import remove_prefix, sql_quote
 
 
-def _kommibelege(additional_conditions=None, limit=None, header_only=False):
-    conditions = ["LKSANB = 0", "LKSTAT<>'X'"]
-    return get_ls_kb_data(conditions, additional_conditions, limit, header_only, is_lieferschein=False)
-
-
 def get_kommibeleg(komminr, header_only=False):
     """Gibt einen Kommissionierbeleg zurück"""
 
@@ -24,7 +19,9 @@ def get_kommibeleg(komminr, header_only=False):
         prefix = 'KB'
     komminr = remove_prefix(komminr, prefix)
 
-    belege = _kommibelege(["LKKBNR = %s" % sql_quote(komminr)], limit=1, header_only=header_only)
+    conditions = ["LKLFSN <> 0", "LKKBNR = %s" % sql_quote(komminr), "LKSTAT<>'X'"]
+    belege = get_ls_kb_data(conditions, additional_conditions=conditions, header_only=header_only,
+                            is_lieferschein=False)
     if belege:
         return belege[0]
     return {}

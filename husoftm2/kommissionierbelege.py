843df65baf90b4b5083a1ef1be8ecb9c5162e1ae
Christian Klein <c.klein@hudora.de>
2011-01-25 08:58:10 +0100

    remove_prefix verwendet, außerdem kennt die Funktion jetzt den alten KB-Präfix

--- husoftm2/kommissionierbelege.py
+++ husoftm2/kommissionierbelege.py
@@ -8,7 +8,7 @@ Copyright (c) 2011 HUDORA. All rights reserved.
 """
 
 from husoftm2.lieferscheine import get_ls_kb_data
-from husoftm2.tools import sql_quote
+from husoftm2.tools import remove_prefix, sql_quote
 
 
 def _kommibelege(additional_conditions=None, limit=None, header_only=False):
@@ -19,9 +19,11 @@ def _kommibelege(additional_conditions=None, limit=None, header_only=False):
 def get_kommibeleg(komminr, header_only=False):
     """Gibt einen Kommissionierbeleg zurück"""
 
-    komminr = str(komminr)
-    if komminr.startswith('KA'):
-        komminr = komminr[2:]
+    prefix = 'KA'
+    if komminr.startswith('KB'):
+        prefix = 'KB'
+    komminr = remove_prefix(komminr, prefix)
+
     belege = _kommibelege(["LKKBNR = %s" % sql_quote(komminr)], limit=1, header_only=header_only)
     if belege:
         return belege[0]

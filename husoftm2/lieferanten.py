ea646bafc28c95281bc214b67a047b244ee27b5d
Christian Klein <c.klein@hudora.de>
2011-12-08 08:23:55 +0100

    Änderungen am Datensatz eines Lieferanten
    Der Rückgabewert soll etwas mehr dem von husoftm2.kunden.get_kunde entsprechen
    
    Change-Id: I7b790198be0c90c2a250b2eab581fbc879c200b8
    Reviewed-on: http://gerrit.hudora.biz/1587
    Tested-by: HudsonAutomation <edv+hudson@hudora.de>
    Tested-by: Maximillian Dornseif <m.dornseif@hudora.de>
    Reviewed-by: Maximillian Dornseif <m.dornseif@hudora.de>

--- husoftm2/lieferanten.py
+++ husoftm2/lieferanten.py
@@ -13,14 +13,16 @@ from husoftm2.tools import pad, remove_prefix, add_prefix, land2iso
 
 def get_address(lieferantennr):
     """Liefert die Adresse zu einer Lieferantennummer."""
-    # Prefix für Lieferanten (SP) entfernen. Für Prefix siehe
+    # Präfix für Lieferanten (SP) entfernen. Für Präfix siehe
     # https://sites.google.com/a/hudora.de/intern/it-administration/nummern/nummernkreise
     lieferantennr = remove_prefix(lieferantennr, 'SP')
     # LISTAT kann 'X' (gelöscht) oder 'I' (inaktiv) sein. Wir wollen nur gültige Adressen, also LISTAT = ' '
-    rows = query('XLI00', condition="LISTAT=' ' AND LIKZLI=1 AND LILINR = %s" % pad('LILINR', lieferantennr))
+    rows = query(tables=['XLI00'],
+                 condition="LISTAT=' ' AND LIKZLI=1 AND LILINR=%s" % pad('LILINR', lieferantennr),
+                 limit=1, ua='husoftm2.lieferanten')
     if rows:
         row = rows[0]
         row['lieferantennr'] = add_prefix(row['lieferantennr'], 'SP')
-        row['land'] = land2iso(row['laenderkennzeichen'])
+        row['land'] = land2iso(row.pop('laenderkennzeichen'))
         return row
     return None

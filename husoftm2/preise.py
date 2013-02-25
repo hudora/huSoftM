aa4620ce8cf17bf015df4dc84958bb389e1209f6
Maximillian Dornseif <md@hudora.de>
2013-02-25 08:27:38 +0100

    Ungenutzte Funktionalitaet entfernt

--- husoftm2/preise.py
+++ husoftm2/preise.py
@@ -149,24 +149,11 @@ def abgabepreis_kunde(artnr, kundennr, auftragsdatum=None):
     return (listenpreis(artnr), 'Listenpreis')
 
 
-def buchdurchschnittspreise(artnrs=None):
-    """Gibt die (aktuellen) Buchdurchschnittspreise in Cent für ein Dict von Artikeln zurück.
-    Wenn keine Artikelnummern angegeben werden, gibt es alle Buchdurchschinttspreise zurück.
-
-    >>> buchdurchschnittspreise(['14600', '14600/00', '14600/01', '14600/02', '14600/03'])
-    {u'14600': 3165,
-     u'14600/00': 0,
-     u'14600/01': 0,
-     u'14600/02': 3132,
-     u'14600/03': 3944}
-    """
-
-    conditions = ["LFLGNR=0",
-                  "LFSTAT<>'X'"]
-    if artnrs:
-        conditions += ["LFARTN IN (%s)" % ','.join([sql_quote(x) for x in artnrs])]
-    rows = query('XLF00', fields=['LFARTN', 'LFPRBD'], condition=' AND '.join(conditions))
-    return dict([(artnr, int(float(preis * 100))) for (artnr, preis) in rows])
+def listenpreis(artnr):
+    """Listenpreis für einene einzelenen Artikel."""
+    preise = listenpreise([artnr]).values()
+    if preise:
+        return preise[0]
 
 
 def listenpreise(artnrs=None):
@@ -192,65 +179,6 @@ def listenpreis(artnr):
         return preise[0]
 
 
-def durchschnittlicher_abgabepreis(artnr, kundennr=None, startdatum=None):
-    """Gibt eine Liste mit den durchschnittlichen Rechnungspreisen pro Monat zurück.
-
-    Liefert eine Liste von 4-Tuples (datum, AVG(preis), menge, umsatz)
-
-    Wenn eine Kundennummer mitgeliefert wird, werden nur Rechungen für diesen Kunden betrachtet.
-    Wenn ein startdatum angegebenw wird, werden nur vorgänge nach diesem Datum betrachtet.
-
-    [ ...
-    (datetime.date(2009, 2, 1), 3295, 2, 6590),
-    (datetime.date(2009, 10, 1), 1744, 2, 3488)]
-
-    Die Funktion ist ausgesprochen langsam - bis zu 8 Sekunden.
-    """
-
-    conditions = [
-        "FUARTN=%s" % (sql_quote(artnr)),  # nur bestimmten Artikel beachten
-        "FKRGNR=FURGNR",   # JOIN
-        "FKAUFA<>'U'",     # Keine Umlagerung
-        "FKSTAT<>'X'",     # nicht gelöscht
-        "FKDTFA>0",        # Druckdatum nicht leer
-        "FUKZRV=2",        # ?
-        "FURGNI<>0",       # ?
-        "FKFORM=' '",      # ?
-        "FURGNR<>0",       # es gibt eine Rechnungsnummer
-        "FUPNET>0",        # keine Gutschriften
-        ]
-
-    if kundennr:
-        kundennr = remove_prefix(kundennr, 'SC')
-        conditions = ["(FKKDNR=%s OR FKKDRG=%s)" % (sql_quote('%8s' % kundennr),
-                                                    sql_quote('%8s' % kundennr))] + conditions
-    if not startdatum:
-        conditions = ["FKDTFA>'10501'"] + conditions  # keine legacy Daten
-    else:
-        conditions = ["FKDTFA>%s" % sql_quote(date2softm(startdatum))[:5]] + conditions
-
-    rows = query(['AFU00', 'AFK00'], fields=["FKDTFA", 'SUM(FUMNG)', 'SUM(FUPNET)', 'COUNT(FKRGNR)'],
-                 condition=' AND '.join(conditions),
-                 grouping='FKDTFA', cachingtime=60 * 60 * 24 * 3,
-                 querymappings={'SUM(FUMNG)': 'menge', 'SUM(FUPNET)': 'nettopreis',
-                                'COUNT(FKRGNR)': 'rechnungen', 'FKDTFA': 'rechnung_date'})
-    mengen = {}
-    umsatz = {}
-    for row in rows:
-        menge = int(float(row['menge']))
-        nettopreis = float(row['nettopreis']) * 100
-        if menge:
-            datum = datetime.date(row['rechnung_date'].year, row['rechnung_date'].month, 1)
-            if datum not in mengen:
-                mengen[datum] = umsatz[datum] = 0
-            mengen[datum] += int(menge)
-            umsatz[datum] += int(nettopreis)
-    ret = []
-    for datum in sorted(mengen.keys()):
-        ret.append((datum, int(umsatz[datum] / mengen[datum]), mengen[datum], umsatz[datum]))
-    return ret
-
-
 def preislisten(gueltig_bis=None):
     """Gib die 'Namen' aller Preislisten für Kundengruppen und Kunden zurück"""
 
@@ -272,7 +200,8 @@ def preislisten(gueltig_bis=None):
         if row['kunde']:
             row['kunde'] = add_prefix(row['kunde'], 'SC')
         if row['preisliste_kunde']:
-            row['preislistenname'] = preislisten[row['preisliste_kunde']]
+            row['preislistenname'] = preislisten.get(row['preisliste_kunde'],
+                                                     "?=%s" % row['preisliste_kunde'])
 
     return rows
 
